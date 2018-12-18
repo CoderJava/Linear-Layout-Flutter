876f2d9d0ae8e5adaee05f6d702291fb7aacf490
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-12-18 13:47:27 +0700

    Create LinearLayout with widget Row, Column, Container, and Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -3,95 +3,123 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("LinearLayout", style: TextStyle(color: Colors.black)),
           backgroundColor: Colors.white,
-        ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(top: 16),
-                        color: Colors.red,
-                        child: Text("Merah",
-                            style: TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(top: 16),
-                        color: Colors.green,
-                        child: Text("Hijau",
-                            style: TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(top: 16),
-                        color: Colors.blue,
-                        child: Text("Biru",
-                            style: TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding: EdgeInsets.only(top: 16),
-                        color: Colors.yellow,
-                        child: Text("Kuning",
-                            style: TextStyle(color: Colors.black),
-                            textAlign: TextAlign.center),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Text("Baris pertama",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 48)),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text("Baris kedua",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 48)),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text("Baris ketiga",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 48)),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Text("Baris keempat",
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 48)),
-                    )
-                  ],
-                ),
-              )
-            ],
+          title: Container(
+            child: Text("LinearLayout",
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.center),
           ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 16),
+                      color: Colors.red,
+                      child: Text("Merah",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 16),
+                      color: Colors.green,
+                      child: Text("Hijau",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 16),
+                      color: Colors.blue,
+                      child: Text("Biru",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.only(top: 16),
+                      color: Colors.yellow,
+                      child: Text("Kuning",
+                          style: TextStyle(color: Colors.black),
+                          textAlign: TextAlign.center),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Baris pertama",
+                          style: TextStyle(fontSize: 48),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Baris kedua",
+                          style: TextStyle(fontSize: 48),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Baris ketiga",
+                          style: TextStyle(fontSize: 48),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Baris keempat",
+                          style: TextStyle(fontSize: 48),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     ));
